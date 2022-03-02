30d8fd30ad79f0773c7e87b656e9cafd4eac617d
cat32tw <mike1993xd@gmail.com>
2022-03-02 21:28:16 +0800

    倒數計時器(Timer練習)

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.lblRSec = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(51, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "倒數的秒數：";
+            // 
+            // txtSec
+            // 
+            this.txtSec.Font = new System.Drawing.Font("新細明體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtSec.Location = new System.Drawing.Point(205, 51);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(100, 28);
+            this.txtSec.TabIndex = 1;
+            this.txtSec.TextChanged += new System.EventHandler(this.txtSec_TextChanged);
+            // 
+            // lblRSec
+            // 
+            this.lblRSec.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRSec.Font = new System.Drawing.Font("新細明體", 55.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblRSec.Location = new System.Drawing.Point(55, 109);
+            this.lblRSec.Name = "lblRSec";
+            this.lblRSec.Size = new System.Drawing.Size(250, 181);
+            this.lblRSec.TabIndex = 2;
+            this.lblRSec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(55, 314);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 38);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(212, 317);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(93, 35);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "重設";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 381);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblRSec);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "倒數計時器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.Label lblRSec;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+
+        int num;
+
+        private void Reset()
+        {
+            txtSec.Focus();
+            txtSec.Clear();
+            lblRSec.Text = "";
+            num = 0;
+            btnStart.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            
+        }
+
+        private void txtSec_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                num = int.Parse(txtSec.Text);
+                lblRSec.Text = "";
+            }
+            catch
+            {
+                Reset();
+                return;
+            }
+
+            if (num < 0)
+            {
+                Reset();
+                return;
+            }
+            else
+            {
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            num--;
+            lblRSec.Text = num.ToString();
+            if (num == 0)
+            {
+                timer1.Enabled = false;
+                btnStart.Enabled = false;
+                MessageBox.Show("時間到！", "倒數計時器", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
